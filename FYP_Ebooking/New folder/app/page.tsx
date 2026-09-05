06003863729fa75622e3b243ac1f6418a91d6bd6
yourharu <nrainlyaa@gmail.com>
2026-09-05 23:00:23 +0800

    Delete FYP_Ebooking directory

--- FYP_Ebooking/New folder/app/page.tsx
+++
@@ -1,6 +0,0 @@
-import { redirect } from 'next/navigation'
-
-export default function Page() {
-  // The booking engine is a standalone vanilla HTML/CSS/JS file served from /public.
-  redirect('/booking.html')
-}
